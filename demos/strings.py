7172b0493d93afe6ce9fedab5d2fed571602c166
Ab Mosca <amosca@westfield.ma.edu>
2024-01-29 07:41:53 -0500

    slides

---
+++ demos/strings.py
@@ -0,0 +1,16 @@
+# animals = ["dog", "cat", "pig"]
+# new_animal = input("Animal? ")
+
+# inList = new_animal in animals 
+
+
+
+nums = [0, 6, -2, 5]
+
+print(min(nums))
+print(max(nums))
+print(sum(nums))
+
+
+animals = ["dog", "cat", "pig"]
+print(len(animals))
