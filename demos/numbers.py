5257631a15d02207d0fb65cf77f8bbefaa4a03c5
Ab Mosca <amosca@westfield.ma.edu>
2024-01-30 14:24:59 -0500

    vidoe

---
+++ demos/numbers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca
+01.29.2024
+
+Notes and demos from class
+"""
+
+# name = input("Type your name ")
+# fave_ani = input("Type your favorite animal ")
+# least_fave_ani = input("Type your least favorite animal ")
+# num = eval(input("Type your favorite number "))
+
+# sen = name + " hates " + least_fave_ani + ", but they love " + fave_ani
+# print(sen)
+
+input_kb = eval(input("Input file size in Kb "))
+
+bytes_in_kb = 1024
+
+input_bytes = bytes_in_kb*input_kb
+
+print("You file is", input_bytes,"bytes")
+
+
+
