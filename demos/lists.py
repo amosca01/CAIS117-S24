b409e4d935eaa02fe301716d555c5ba9ca12e645
Ab Mosca <amosca@westfield.ma.edu>
2024-02-05 12:20:57 -0500

    class

---
+++ demos/lists.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca
+02.05.24
+
+Class list activity 
+"""
+
+# 1 Get names from user
+
+
+# 2 Make input a list 
+
+
+# 3 Print length of list 
+
+# 4 Print in alphabetical order
+
+# 5 Print in reverce alphabetical order
+
