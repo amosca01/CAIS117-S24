c938394bbb9df99f10e72d629d6da60d4d3ebae9
Ab Mosca <amosca@westfield.ma.edu>
2024-02-05 10:10:31 -0500

    class

---
+++ demos/strings_demo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca
+
+02.02.2024
+
+Class Demo 02.02
+"""
+
+s = "BATMAN AND ROBIN" 
+
+# BATMAN
+print(s[0:6])
+
+# ROBIN
+print(s[11:16])
+
+# BATBIN
+print(s[0:3]+s[13:16])
+
+# ROB ATM
+print(s[11:14], s[1:4]) 
+
+# user input
+s = input("Input a word ")
