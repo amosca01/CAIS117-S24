d2d868a21b52586a16f5a07ccdb2a2f807087002
Ab Mosca <amosca@westfield.ma.edu>
2024-02-09 12:18:17 -0500

    demo

---
+++ demos/conditionals.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca 
+02/09/2024
+
+Conditional class notes
+"""
+
+# num_sunny_days = eval(input("How many sunny days? "))
+# good_month = num_sunny_days >15
+
+# if good_month:
+#     print("It's a great month!")
+# else:
+#     print("It's a bad month")
+
+num = eval(input("Input a number between -10 and 10 "))
+
+# print even or odd 
+if (num % 2 == 0):
+    print("Even!")
+else: 
+    print("Odd!")
+    
+# print pos, neg, or nothing
+if (num > 0):
+    print("Positive!")
+elif (num < 0):
+    print("Negative!")   
+else:
+    print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
