1bb40ac53a81a5df50ca13cd3c1b65d1b686aa20
Ab Mosca <amosca@westfield.ma.edu>
2024-02-09 07:21:16 -0500

    slides

---
+++ demos/q02.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 17:07:08 2024
+
+@author: abmosca
+
+Quiz 01
+"""
+
+
+
+
+# word = input("Type a word: ")
+# wordLen = len(word)
+# top_bottom = "#" * (wordLen + 4)
+# middle = "# " + word + " #"
+
+# print(top_bottom)
+# print(middle)
+# print(top_bottom)
+
+# print(type(word))
+
+# input("Type a number: ")
+
+# import math
+
+# print(math.ceil(3.4))
+
+# num = 23 // 4
+# print(num)
+
+# s = "I love coding"
+# print(s[:-6])
+
+# s = "cats,bats,mats"
+# l = s.split(",")
+# print(l[2])
+
+classList = ["Ab", "Steve", "Henri"]
+newStudent = input("Type new student's name: ")
+inClass = [a] [b] [c]
+
+print(inClass)
+
