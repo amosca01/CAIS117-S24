03800bed6ed020096cdc9069f19a6919166cf995
Ab Mosca <amosca@westfield.ma.edu>
2024-02-26 12:15:52 -0500

    class

---
+++ demos/for.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca 
+02.26.24
+
+Class notes on for loops
+"""
+
+# # sum a list of numbers from the user
+# numbers = input("Enter a list of numbers, separated by commas ")
+# numbers = numbers.split(",")
+
+# total = 0
+
+# for number in numbers:
+#     total = total + int(number)
+    
+# print(total)
+
+# for i in [1, 2, 3, 4, 5]:
+#     print("hello!")
+
+# # print integers from 10 to 1
+# for i in range(100, 0, -1):
+#     print("hello!")
+
+# conversion table
+for i in range(100, -30, -10):
+    celcius = (i-32)*5/9
+    print(i, "F is", celcius, "C")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
