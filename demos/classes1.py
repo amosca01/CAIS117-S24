89c83447c019ad5c1af142facfc20e91eda4f717
Ab Mosca <amosca@westfield.ma.edu>
2024-03-22 12:21:29 -0400

    class

--- demos/classes1.py
+++ demos/classes1.py
@@ -7,6 +7,8 @@ Classes Pt1 Notes
 Ab Mosca
 """
 
+import random 
+
 class Playlist:
     
     def __init__(self):
@@ -16,11 +18,25 @@ class Playlist:
     def add(self, song):
         self.songs.append(song)
     
-    # REMOVE
-    
     # PRINT
+    def printSongs(self):
+        for song in self.songs:
+            print("Title:", song["title"],
+                  "Artist:", song["artist"],
+                  "Album:", song["album"])
+            
+# Radio 
+class Radio(Playlist):
     
-    # SEARCH 
+    def __init__(self, lib):
+        numSongs = len(lib) - 1
+        # randomly choose 10 songls from library 
+        songs = []
+        for i in range(0, 10):
+            index = random.randint(0, numSongs)
+            song = lib[index]
+            songs.append(song)
+        self.songs = songs
     
 def main():
     myPlaylist = Playlist()
@@ -28,7 +44,18 @@ def main():
            "artist": "unknown",
            "album": "Birthday"}
     myPlaylist.add(hbd)
+    myPlaylist.printSongs()
     
+    # make a library of 100 
+    # pretend songs 
+    library = []
+    for i in range(0, 100):
+        newSong = {
+            "title": "title_"+str(i),
+            "artist": "artist_"+str(i),
+            "album": "album_"+str(i)
+            }
+        library.append(newSong)
     
 if __name__ == "__main__":
     main() 
---
+++ demos/classes3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ab Mosca
+03.22.24
+
+Classes Day 3 Notes
+"""
+
+
+
