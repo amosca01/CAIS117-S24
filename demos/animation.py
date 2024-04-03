402b554ac9eb1bebdc4adf60dedef3185a3a3713
Ab Mosca <amosca@westfield.ma.edu>
2024-04-03 10:29:00 -0400

    class

--- demos/animation.py
+++ demos/animation.py
@@ -1,10 +1,28 @@
 from graphics import *
+
 def main():
   win = GraphWin("My Circle", 600, 400)
   c = Circle(Point(50,50), 10)
+  c.setFill("red")
   c.draw(win)
-  for i in range(150):
-    c.move(3,2) # The move function takes two values, dx and dy
+  #while win.checkMouse() == None:
+  for i in range(200): 
+    
+    dx = 3
+    dy = 2
+    c.move(dx,dy) # The move function takes two values, dx and dy
+    
+    # Add code to make the ball bounce
+    # get X and get Y
+    currX = c.getCenter().getX()
+    currY = c.getCenter().getY()
+    if currX > 600:
+        dx = -dx
+    if currY > 400:
+        dy = -dy
+    
   win.getMouse() # Pause to view result
   win.close() # Close window when done
-main()
+
+if __name__ == "__main__":
+    main()
