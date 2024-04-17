b6c267b4340bc4e5c376a8f3ef6118173197286f
Ab Mosca <amosca@westfield.ma.edu>
2024-04-17 11:11:09 -0400

    files

---
+++ demos/read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+04.17.24
+Ab Mosca
+
+File to demo reading txt files 
+"""
+
+def main():
+    
+
+    
+if __name__ == "__main__":
+    main()
+
